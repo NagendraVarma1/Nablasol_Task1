f09991c7117cc7087e71e9a45787d7ab484ef744
NagendraVarma1 <nagendravarmal512@gmail.com>
2024-06-09 17:52:07 +0530

    storing in local storage

--- src/Components/FirstPage.js
+++ src/Components/FirstPage.js
@@ -1,19 +1,42 @@
 import { Button, Col, Form, FormSelect, Row } from "react-bootstrap";
 import Card from "./Card";
+import { useState } from "react";
 
 const FirstPage = ({onNext}) => {
-  return (
+    const [projectName, setProjectName] = useState('')
+    const [client, setClient] = useState('')
+    const [newClient, setNewClient] = useState('')
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('')
+    const [text, setText] = useState('')
+
+    const firstPageData = {
+        name: projectName,
+        client: client,
+        newClient: newClient,
+        startDate: startDate,
+        endDate: endDate,
+        text: text
+    }
+
+    const nextPageHandler = () => {
+        localStorage.setItem('First Page Data', JSON.stringify(firstPageData))
+        onNext()
+    }
+
+
+   return (
     <Card>
       <h3 style={{ textAlign: "center" }}>Create a Project</h3>
       <Form style={{ width: "80%", marginLeft: "10%", marginBottom: '15px' }}>
         <Form.Group className="mb-3">
           <Form.Label>Project Name</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" name="projectName" onChange={(e) => setProjectName(e.target.value)} />
         </Form.Group>
         <Form.Group as={Row} className="mb-3">
           <Form.Label>Client</Form.Label>
           <Col lg="8" sm="12">
-            <FormSelect>
+            <FormSelect onChange={(e) => setClient(e.target.value)}>
               <option>Select a Client</option>
               <option value="Client 1">Client 1</option>
               <option value="Client 2">Client 2</option>
@@ -21,25 +44,25 @@ const FirstPage = ({onNext}) => {
             </FormSelect>
           </Col>
           <Col lg="4" sm="12">
-            <Form.Control type="text" placeholder="Add New Client" />
+            <Form.Control type="text" placeholder="Add New Client" onChange={(e) => setNewClient(e.target.value)} />
           </Col>
         </Form.Group>
         <Form.Group as={Row} className="mb-3">
           <Form.Label>Dates</Form.Label>
           <Col lg="6">
-            <Form.Control type="date" />
+            <Form.Control type="date" onChange={(e) => setStartDate(e.target.value)} />
           </Col>
           <Col lg="6">
-            <Form.Control type="date" />
+            <Form.Control type="date" onChange={(e) => setEndDate(e.target.value)}/>
           </Col>
         </Form.Group>
         <Form.Group className="mb-3">
             <Form.Label>Notes</Form.Label>
-            <Form.Control as="textarea"/>
+            <Form.Control as="textarea" onChange={(e) => setText(e.target.value)}/>
         </Form.Group>
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
             <Button variant="outline-secondary">Back</Button>
-            <Button onClick={onNext}>Next</Button>
+            <Button onClick={nextPageHandler}>Next</Button>
         </div>
       </Form>
       <p style={{textAlign: 'center', fontSize: 'small'}}>page 1</p>
--- src/Components/SecondPage.js
+++ src/Components/SecondPage.js
@@ -1,58 +1,84 @@
 import { Button, Col, Form, Row } from "react-bootstrap";
 import Card from "./Card";
-import classes from './SecondPage.module.css'
+import classes from "./SecondPage.module.css";
+import { useState } from "react";
 
 const SecondPage = ({ onNext, onBack }) => {
+  const [projectType, setprojectType] = useState("");
+  const [hourlyRate, setHourlyRate] = useState('')
+
+  const secondPageData = {
+    projectType: projectType,
+    hourlyRate: hourlyRate,
+  }
+
+  const nextPageHandler = () => {
+    localStorage.setItem('Second Page Data', JSON.stringify(secondPageData))
+  }
   return (
     <Card>
       <h3 style={{ textAlign: "center" }}>Project Type</h3>
       <p style={{ textAlign: "center" }}>
         Don't panic - You can also Customize this type in settings
       </p>
-      <div style={{ width: "80%", marginLeft: "10%"}}>
-        <Button variant="outline-primary" className={classes.btn}>Fixed Fee</Button>
-        <Button variant="outline-primary" className={classes.btn}>Time & Material</Button>
-        <Button variant="outline-primary" className={classes.btn}>Non-Billable</Button>
+      <div style={{ width: "80%", marginLeft: "10%" }}>
+        <Button
+          variant="outline-primary"
+          className={classes.btn}
+          onClick={() => setprojectType("Time & Material")}
+        >
+          Time & Material
+        </Button>
+        <Button
+          variant="outline-primary"
+          className={classes.btn}
+          onClick={() => setprojectType("Fixed Fee")}
+        >
+          Fixed Fee
+        </Button>
+        <Button
+          variant="outline-primary"
+          className={classes.btn}
+          onClick={() => setprojectType("Non-Billable")}
+        >
+          Non-Billable
+        </Button>
       </div>
-      <Form style={{ width: "80%", marginLeft: "10%", marginTop:'10px'}}>
+      <Form style={{ width: "80%", marginLeft: "10%", marginTop: "10px" }}>
         <Form.Group as={Row}>
-            <Form.Label style={{fontWeight: 'bold'}}>Hourly</Form.Label>
-            <p>We need hourly rates to track your Project's billable amount</p>
-            <Col lg='6'>
-                <Form.Select>
-                    <option>Project Hourly Rate</option>
-                </Form.Select>
-            </Col>
-            <Col lg='4'>
-                <Form.Control type="number" placeholder="In Rs."/>
-            </Col>
+          <Form.Label style={{ fontWeight: "bold" }}>Hourly</Form.Label>
+          <p>We need hourly rates to track your Project's billable amount</p>
+          <Col lg="6">
+            <Form.Select>
+              <option>Project Hourly Rate</option>
+            </Form.Select>
+          </Col>
+          <Col lg="4">
+            <Form.Control type="number" placeholder="In Rs." onChange={(e) => setHourlyRate(e.target.value)} />
+          </Col>
         </Form.Group>
 
         <Form.Group as={Row}>
-            <Form.Label style={{fontWeight: 'bold'}}>Budget</Form.Label>
-            <p>We need hourly rates to track your Project's billable amount</p>
-            <Col lg='6'>
-                <Form.Select>
-                    <option>Hours per Person</option>
-                </Form.Select>
-            </Col>
-            <Col lg='4'>
-                <Form.Control type="number" placeholder="In Rs."/>
-            </Col>
+          <Form.Label style={{ fontWeight: "bold" }}>Budget</Form.Label>
+          <p>We need hourly rates to track your Project's billable amount</p>
+          <Col lg="6">
+            <Form.Select>
+              <option>Hours per Person</option>
+            </Form.Select>
+          </Col>
         </Form.Group>
-            <Form.Check className="mt-4" label="Budget resets every month"/>
-            <Form.Check className="mt-4 mb-3" label="Send Email alerts if project exceeds 80% of budget"/>
+        <Form.Check className="mt-4" label="Budget resets every month" />
+        <Form.Check
+          className="mt-4 mb-3"
+          label="Send Email alerts if project exceeds 80% of budget"
+        />
       </Form>
-
-
-
-
 
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Button variant="outline-secondary" onClick={onBack}>
           Back
         </Button>
-        <Button onClick={onNext}>Next</Button>
+        <Button onClick={nextPageHandler}>Next</Button>
       </div>
       <p style={{ textAlign: "center", fontSize: "small" }}>page 2</p>
     </Card>
